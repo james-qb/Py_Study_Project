4f5a1cc2168e2c166606432be3bc17a8e812b740
qiubin <384341396@qq.com>
2021-03-29 18:00:06 +0800

    test

---
+++ Day_001/Sty/test_pyech.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/26 15:04
+# @Author  : qiubin
+# @File    : test_pyech.py
+# @Software: PyCharm
---
+++ Day_001/py_echart/test_bar.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/26 16:51
+# @Author  : qiubin
+# @File    : test_bar.py
+# @Software: PyCharm
